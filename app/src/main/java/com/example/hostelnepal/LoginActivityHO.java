5b8eb2224bcc3c540859ba1261e8f88b354bdaef
utsav <utsavbudathoki12@gmail.com>
2020-03-02 20:33:53 +0545

    Second commit

--- app/src/main/java/com/example/hostelnepal/LoginActivityHO.java
+++ app/src/main/java/com/example/hostelnepal/LoginActivityHO.java
@@ -1,14 +1,40 @@
 package com.example.hostelnepal;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivityHO extends AppCompatActivity {
+
+    FirebaseAuth fAuth;
+    EditText hoEmail,hoPassword;
+    Button hoLoginBtn;
+    ProgressBar progressBar;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_ho);
+        hoLoginBtn = findViewById(R.id.loginho);
+
+       
     }
+
+
 }
